c6d16b11e08fd905d4d7fd378a5ba413736f6aa7
nesscollege2016 <nesscollege2016@gmail.com>
2016-02-18 20:42:10 +0200

    print a triangle with nested loops

--- src/edu/tomer/ness/Main.java
+++ src/edu/tomer/ness/Main.java
@@ -7,28 +7,21 @@ public class Main {
 
     public static void main(String[] args) {
         //define the scanner once in each program
-        Scanner scanner = new Scanner(System.in);
 
-        //input a number:
-        System.out.println("How many fibonacci numbers?:");
-        int n = scanner.nextInt();
+        int numRows = 3;
+        int numCol = 4;
 
-        int fib1 = 0;
-        int fib2 = 1;
-        int counter = 2;
-        System.out.println(fib1);
-        System.out.println(fib2);
-        //1  2  3  4  5  6  7
-        //0, 1, 1, 2, 3, 5, 8
+        int i = 0;
+        int j = 0;
 
-        //n = 6
-        while (counter < n){
-            int temp = fib2; //temp = 2
-            fib2 = fib1 + fib2; // fib2 = 3
-            fib1 = temp;// fib1 = 2
-            System.out.println(fib2);
-            counter++;
+        while (i < numRows){
+            while (j <= i ){
+                j++;
+                System.out.print("*");
+            }
+            j = 0;
+            System.out.println();
+            i++;
         }
-
     }
 }
